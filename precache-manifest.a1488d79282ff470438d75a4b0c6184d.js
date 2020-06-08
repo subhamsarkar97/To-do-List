ed5ba58d0b8b56443d8256918b7796a2bf38aafa
subhamsarkar97 <45004532+subhamsarkar97@users.noreply.github.com>
2020-06-08 22:40:57 +0530

    Updates

---
+++ precache-manifest.a1488d79282ff470438d75a4b0c6184d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0680ef0766d81245ed15a6f772b03b00",
+    "url": "/To-do-List/index.html"
+  },
+  {
+    "revision": "72c6dc381d80b3307aca",
+    "url": "/To-do-List/static/css/main.c2c1c1e4.chunk.css"
+  },
+  {
+    "revision": "7039667b7fd2f4ff97cc",
+    "url": "/To-do-List/static/js/2.9f527bc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-List/static/js/2.9f527bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c6dc381d80b3307aca",
+    "url": "/To-do-List/static/js/main.bae1c78a.chunk.js"
+  },
+  {
+    "revision": "0bbc6f584f2c3731015e",
+    "url": "/To-do-List/static/js/runtime-main.55f147fd.js"
+  }
+]);
